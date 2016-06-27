a3224e9e5128fbb841cba2a59151293face73c75
Eric Sowell <eric.sowell@gmail.com>
2016-06-26 22:43:42 -0500

    added PUT and DELETE

--- README.md
+++ README.md
@@ -14,7 +14,12 @@
 * Using Heroku's port
 * Creating Heroku app
 
-## Creating a JSON api
+## Creating a Basic JSON api
 
 * Create a basic GET and POST api
 * POST with CURL - `curl --data "description=wat" http://localhost:8000/api/todo`
+
+## Adding PUT and DELETE to the api
+
+* Add PUT and DELETE
+* PUT with CURL - `curl -X PUT -d isComplete=false -d description=new http://localhost:8000/api/todo/538350`
--- api/todo.js
+++ api/todo.js
@@ -28,9 +28,44 @@ let post = {
   }
 }
 
+let put = {
+  method: 'PUT',
+  path: '/api/todo/{id}',
+  handler: function(request, response) {
+    let id = encodeURIComponent(request.params.id);
+    let description = request.payload.description;
+    let isComplete = request.payload.isComplete || false;
+
+    let first = todos.find(x => { return x.id == id; });
+    if (first) {
+      first.description = description;
+      first.isComplete = isComplete;
+    }
+
+    response();
+  }
+}
+
+let destroy = {
+  method: 'DELETE',
+  path: '/api/todo/{id}',
+  handler: function(request, response) {
+    let id = encodeURIComponent(request.params.id);
+    let index = todos.findIndex(x => { return x.id == id; });
+    
+    if (index > -1) {
+      todos.splice(index, 1);
+    }
+    response();
+  }
+}
+
+
 module.exports = {
   registerApi: function(server) {
     server.route(get);
     server.route(post);
+    server.route(put);
+    server.route(destroy);
   }
 }
